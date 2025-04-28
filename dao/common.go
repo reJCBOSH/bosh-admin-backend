28933920126e09d86a6115ba46473e520e408523
JCBOSH <942419633@qq.com>
2025-04-28 20:36:49 +0800

    feat: 新增数据库公用方法

---
+++ dao/common.go
@@ -0,0 +1,109 @@
+package dao
+
+import (
+	"errors"
+	"fmt"
+
+	"bosh-admin/global"
+
+	"gorm.io/gorm"
+)
+
+// GormDB 获取gorm.DB
+func GormDB() *gorm.DB {
+	return global.GormDB
+}
+
+// Query 原生sql查询
+func Query(sql string, values ...interface{}) *gorm.DB {
+	return GormDB().Raw(sql, values...)
+}
+
+// Exec 原生sql执行
+func Exec(sql string, values ...interface{}) *gorm.DB {
+	return GormDB().Exec(sql, values...)
+}
+
+// Begin 开启事务
+func Begin() *gorm.DB {
+	return GormDB().Begin()
+}
+
+func Create(value interface{}) *gorm.DB {
+	return GormDB().Create(value)
+}
+
+func Save(value interface{}) *gorm.DB {
+	return GormDB().Save(value)
+}
+
+func QueryList[T Basic](model T, s *Statement) (data []T, total int64, err error) {
+	DB := s.Format().Model(&model)
+	s.fields.Range(func(query interface{}, args []interface{}) bool {
+		DB = DB.Select(query, args...)
+		return true
+	})
+	s.joins.Range(func(query string, args []interface{}) bool {
+		DB = DB.Joins(query, args...)
+		return true
+	})
+	err = DB.Count(&total).Error
+	if err != nil {
+		return
+	}
+	DB = DB.Scopes(OtherScope(s.other))
+	s.preloads.Range(func(query string, args []interface{}) bool {
+		DB = DB.Preload(query, args...)
+		return true
+	})
+	err = DB.Find(&data).Error
+	return
+}
+
+func QueryOne[T Basic](model T, s *Statement) (data T, err error) {
+	DB := s.Format().Model(&model)
+	s.fields.Range(func(query interface{}, args []interface{}) bool {
+		DB = DB.Select(query, args...)
+		return true
+	})
+	s.joins.Range(func(query string, args []interface{}) bool {
+		DB = DB.Joins(query, args...)
+		return true
+	})
+	s.preloads.Range(func(query string, args []interface{}) bool {
+		DB = DB.Preload(query, args...)
+		return true
+	})
+	err = DB.First(&data).Error
+	return
+}
+
+// QueryById 通过id查询
+func QueryById[T any](id any) (data T, err error) {
+	err = GormDB().First(&data, id).Error
+	return
+}
+
+// DelById 通过id删除
+func DelById[T any](id any) error {
+	model := new(T)
+	return GormDB().Delete(model, id).Error
+}
+
+// Count 统计数量
+func Count[T Basic](model T, s *Statement) (num int64, err error) {
+	err = s.Format().Model(&model).Count(&num).Error
+	return
+}
+
+// Sum 求和
+func Sum[T Basic](model T, s *Statement) (num float64, err error) {
+	fields := s.fields.Keys()
+	if len(fields) == 0 || fields[0] == "" {
+		err = errors.New("求和字段错误")
+		return
+	}
+	field := fields[0]
+	err = s.Format().Model(&model).Pluck(fmt.Sprintf("COALESCE(SUM(%s), 0)", field), &num).Error
+	return
+}
