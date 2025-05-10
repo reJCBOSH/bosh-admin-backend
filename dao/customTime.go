36148e7e39995e7b7b4c20e8288aedc201994660
JCBOSH <942419633@qq.com>
2025-05-10 17:55:17 +0800

    feat: 新增自定义日期类型

--- dao/customTime.go
+++ dao/customTime.go
@@ -60,3 +60,57 @@ func (t CustomTime) ToTime() time.Time {
 	year, month, day := tTime.Date()
 	return time.Date(year, month, day, tTime.Hour(), tTime.Minute(), tTime.Second(), 0, time.Local)
 }
+
+// CustomDate 自定义日期类型
+type CustomDate time.Time
+
+func (t CustomDate) MarshalJSON() ([]byte, error) {
+	if time.Time(t).IsZero() {
+		return []byte("\"\""), nil
+	}
+	b := make([]byte, 0, len(time.DateOnly)+2)
+	b = append(b, '"')
+	b = time.Time(t).AppendFormat(b, time.DateOnly)
+	b = append(b, '"')
+	return b, nil
+}
+
+func (t *CustomDate) UnmarshalJSON(data []byte) (err error) {
+	if len(data) > 2 && data[0] == '"' {
+		now, err := time.ParseInLocation(`"`+time.DateOnly+`"`, string(data), time.Local)
+		*t = CustomDate(now)
+		return err
+	}
+	return
+}
+
+func (t CustomDate) Value() (driver.Value, error) {
+	var zeroTime time.Time
+	tTime := time.Time(t)
+	// 判断给定时间是否和默认零时间的时间戳相同
+	if tTime.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return tTime, nil
+}
+
+func (t *CustomDate) Scan(v any) error {
+	if value, ok := v.(time.Time); ok {
+		*t = CustomDate(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t CustomDate) String() string {
+	return time.Time(t).Format(time.DateOnly)
+}
+
+func (t CustomDate) ToTime() time.Time {
+	tTime := time.Time(t)
+	if tTime.IsZero() {
+		return tTime
+	}
+	year, month, day := tTime.Date()
+	return time.Date(year, month, day, 0, 0, 0, 0, time.Local)
+}
