096cc1d6e2631056e97874b3bba1d7de3e848c5a
JCBOSH <942419633@qq.com>
2025-05-10 14:14:56 +0800

    refactor: 响应结构移至ctx

--- core/ctx/response.go
+++ core/ctx/response.go
@@ -3,14 +3,31 @@ package ctx
 import (
 	"net/http"
 
-	"bosh-admin/dao/dto"
 	"bosh-admin/exception"
 	"bosh-admin/global"
 )
 
+const (
+	SUCCESS = true
+	FAIL    = false
+)
+
+// Response 响应结构体
+type Response struct {
+	Success bool   `json:"success"`
+	Data    any    `json:"data"`
+	Msg     string `json:"msg"`
+}
+
+// ListData 列表数据
+type ListData struct {
+	List  any   `json:"list"`
+	Total int64 `json:"total"`
+}
+
 // Response 响应
 func (c *Context) Response(success bool, data any, msg string) {
-	c.JSON(http.StatusOK, dto.Response{
+	c.JSON(http.StatusOK, Response{
 		Success: success,
 		Data:    data,
 		Msg:     msg,
@@ -23,17 +40,17 @@ func (c *Context) Success(msg ...string) {
 	if len(msg) > 0 {
 		exceptionMsg = msg[0]
 	}
-	c.Response(dto.SUCCESS, nil, exceptionMsg)
+	c.Response(SUCCESS, nil, exceptionMsg)
 }
 
 // SuccessWithData 成功数据响应
 func (c *Context) SuccessWithData(data any) {
-	c.Response(dto.SUCCESS, data, exception.Success)
+	c.Response(SUCCESS, data, exception.Success)
 }
 
 // SuccessWithList 成功列表响应
 func (c *Context) SuccessWithList(list any, total int64) {
-	c.Response(dto.SUCCESS, dto.ListData{
+	c.Response(SUCCESS, ListData{
 		List:  list,
 		Total: total,
 	}, exception.Success)
@@ -41,16 +58,16 @@ func (c *Context) SuccessWithList(list any, total int64) {
 
 // SuccessWithDetail 成功详情响应
 func (c *Context) SuccessWithDetail(data any, msg string) {
-	c.Response(dto.SUCCESS, data, msg)
+	c.Response(SUCCESS, data, msg)
 }
 
 // Fail 失败响应
 func (c *Context) Fail(msg ...string) {
 	var exceptionMsg = exception.ServerError
 	if len(msg) > 0 {
-		c.Response(dto.FAIL, nil, msg[0])
+		exceptionMsg = msg[0]
 	}
-	c.Response(dto.FAIL, nil, exceptionMsg)
+	c.Response(FAIL, nil, exceptionMsg)
 }
 
 // HandlerError 错误处理
@@ -65,8 +82,8 @@ func (c *Context) HandlerError(err any, msg ...string) bool {
 
 // UnAuthorized 未授权响应
 func (c *Context) UnAuthorized(msg string) {
-	c.JSON(http.StatusUnauthorized, dto.Response{
-		Success: dto.FAIL,
+	c.JSON(http.StatusUnauthorized, Response{
+		Success: FAIL,
 		Data:    nil,
 		Msg:     msg,
 	})
