db9c80d9eba7121372ef6dfac021e373afcfb5dd
JCBOSH <942419633@qq.com>
2025-05-10 14:13:59 +0800

    Revert "refactor: 响应结构体移至ctx"
    This reverts commit 6fe280057164f6cb2d8c7d7c9f461bef3b0a73aa.

---
+++ dao/dto/response.go
@@ -0,0 +1,19 @@
+package dto
+
+const (
+	SUCCESS = true
+	FAIL    = false
+)
+
+// Response 响应结构体
+type Response struct {
+	Success bool   `json:"success"`
+	Data    any    `json:"data"`
+	Msg     string `json:"msg"`
+}
+
+// ListData 列表数据
+type ListData struct {
+	List  any   `json:"list"`
+	Total int64 `json:"total"`
+}
