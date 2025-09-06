b70a2575f3925127b58c57042dfcd5f04cd3b0e0
JCBOSH <942419633@qq.com>
2025-09-06 12:00:13 +0800

    chore: nothing change

--- global/global.go
+++ global/global.go
@@ -1,33 +1,33 @@
 package global
 
 import (
-    "bosh-admin/config"
+	"bosh-admin/config"
 
-    "github.com/gin-gonic/gin"
-    ut "github.com/go-playground/universal-translator"
-    "github.com/lionsoul2014/ip2region/binding/golang/xdb"
-    "go.uber.org/zap"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/lionsoul2014/ip2region/binding/golang/xdb"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 // 环境常量
 const (
-    DEV  = "dev"
-    UAT  = "uat"
-    PROD = "prod"
+	DEV  = "dev"
+	UAT  = "uat"
+	PROD = "prod"
 )
 
 var (
-    Config      config.Config      // 配置
-    Logger      *zap.SugaredLogger // 日志
-    Trans       ut.Translator      // 翻译器
-    GormDB      *gorm.DB           // gorm数据库
-    Router      *gin.Engine        // 路由
-    XdbSearcher *xdb.Searcher      // 全局xdb搜索器
+	Config      config.Config      // 配置
+	Logger      *zap.SugaredLogger // 日志
+	Trans       ut.Translator      // 翻译器
+	GormDB      *gorm.DB           // gorm数据库
+	Router      *gin.Engine        // 路由
+	XdbSearcher *xdb.Searcher      // 全局xdb搜索器
 )
 
 const (
-    SuperAdmin      = "SuperAdmin"
-    SystemAdmin     = "SystemAdmin"
-    DefaultPassword = "Ab112112."
+	SuperAdmin      = "SuperAdmin"
+	SystemAdmin     = "SystemAdmin"
+	DefaultPassword = "Ab112112."
 )
--- go.mod
+++ go.mod
@@ -49,7 +49,6 @@ require (
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
